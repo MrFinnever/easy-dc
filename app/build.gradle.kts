6d9ef1d2513e392e83c34f9e74131feb94032e21
Leonid Krupenkov <mr.finnever@gmail.com>
2024-12-12 00:31:08 +0300

    + [Version] 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.1.0"
         resConfigs("en", "ru")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
