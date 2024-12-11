c5bae612a464185aae39ca5b61b984627d97b882
Leonid Krupenkov <mr.finnever@gmail.com>
2024-12-12 00:26:48 +0300

    + [UI] Color Schemes

---
+++ app/src/main/java/net/kep/easy_dc/ui/theme/ColorSchemes.kt
@@ -0,0 +1,155 @@
+package net.kep.easy_dc.ui.theme
+
+import androidx.compose.ui.graphics.Color
+import net.kep.easy_dc.data.theme.CustomColors
+
+internal val LightColorScheme = CustomColors(
+
+    //General
+    background = Color(0xFFF1F1F1),
+    onBackground = Color(0xFF000000),
+
+    surface = Color(0xFFFFFFFF),
+    onSurface = Color(0xFF000000),
+
+    //NavBar
+    primary = Color(0xFF2196F3),
+    onPrimary = Color(0xFFFFFFFF),
+
+    //Calculator
+    elementOnSurface = Color(0xFF949494),
+
+    fabContainer = Color(0xFF000000),
+    onFabContainer = Color(0xFFFFFFFF),
+
+    //Result
+    resultCardAccent = Color(0xFF2196F3),
+    onResultCardAccent = Color(0xFFFFFFFF),
+
+    //Solution
+    checkBox = Color(0xff4ccf00),
+    onCheckBox = Color(0xFFFFFFFF),
+
+    isInverseBox = Color(0xFF81C784),
+    isNotInverseBox = Color(0xFFEF9A9A),
+    onInverseBox = Color(0xFFFFFFFF),
+
+    secondaryCard = Color(0xFFEEEEEE),
+    onSecondaryCard = Color(0xFF555555),
+
+    nodeCircle = Color.Gray,
+    onNodeCircle = Color(0xFFFFFFFF),
+
+    //Settings
+    settingsTextIndicator = Color(0xFF42A5F5),
+    overlineText = Color(0xFF555555),
+    divider = Color(0xFFEEEEEE),
+
+
+//    primary = Color(0xFF90CAF9),
+//    onPrimary = Color(0xFFFFFFFF),
+//    primaryContainer = Color(0xFF2196F3),
+//    onPrimaryContainer = Color(0xFFFFFFFF),
+//
+//    secondary = Color(0xFF5C6BC0),
+//    onSecondary = Color(0xFF000000),
+//    secondaryContainer = Color(0xFFB3B3B3),
+//    onSecondaryContainer = Color(0xFFFFFFFF),
+//
+//    elementOnSurface = Color(0xFF949494),
+//    settingsTextIndicator = Color(0xFF42A5F5),
+//
+//    error = Color(0xFFB00020),
+//    onError = Color(0xFFFFFFFF),
+//    errorContainer = Color(0xFFFFDAD4),
+//    onErrorContainer = Color(0xFF410002),
+//
+//    background = Color(0xFFF2F3F5),
+//    onBackground = Color(0xFF000000),
+//    surface = Color(0xFFFFFFFF),
+//    onSurface = Color(0xFF000000),
+//    surfaceVariant = Color(0xFFEEEEEE),
+//    onSurfaceVariant = Color(0xFF555555),
+//
+//    outline = Color(0xFFB0BEC5),
+//    inverseSurface = Color(0xFF121212),
+//    inverseOnSurface = Color(0xFFFFFFFF),
+//    inversePrimary = Color(0xFF3700B3)
+)
+
+internal val DarkColorScheme = CustomColors(
+    //General
+    background = Color(0xFF121212),
+    onBackground = Color(0xFFFFFFFF),
+
+    surface = Color(0xFF2B2B2B),
+    onSurface = Color(0xFFFFFFFF),
+
+    //NavBar
+    primary = Color(0xFF1E88E5),
+    onPrimary = Color(0xFFFFFFFF),
+
+    //Calculator
+    elementOnSurface = Color(0xFF949494),
+
+    fabContainer = Color(0xFFFFFFFF),
+    onFabContainer = Color(0xFF000000),
+
+    //Result
+    resultCardAccent = Color(0xFFBBDEFB),
+    onResultCardAccent = Color(0xFF000000),
+
+    //Solution
+    checkBox = Color(0xff4ccf00),
+    onCheckBox = Color(0xFFFFFFFF),
+
+    isInverseBox = Color(0xFF81C784),
+    isNotInverseBox = Color(0xFFEF9A9A),
+    onInverseBox = Color(0xFFFFFFFF),
+
+    secondaryCard = Color(0xFF525252),
+    onSecondaryCard = Color(0xFFE9E9E9),
+
+    nodeCircle = Color.Gray,
+    onNodeCircle = Color(0xFFFFFFFF),
+
+    //Settings
+    settingsTextIndicator = Color(0xFFBBDEFB),
+    overlineText = Color(0xFFE9E9E9),
+    divider = Color(0xFF525252),
+
+
+
+
+
+//
+//    primary = Color(0xFF42A5F5),
+//    onPrimary = Color(0xFFFFFFFF),
+//    primaryContainer = Color(0xFF1E88E5),
+//    onPrimaryContainer = Color(0xFFFFFFFF),
+//
+//    secondary = Color(0xFF42A5F5),
+//    onSecondary = Color(0xFF000000),
+//    secondaryContainer = Color(0xFF80CBC4),
+//    onSecondaryContainer = Color(0xFF000000),
+//
+//    elementOnSurface = Color(0xFF949494),
+//    settingsTextIndicator = Color(0xFFBBDEFB),
+//
+//    error = Color(0xFFCF6679),
+//    onError = Color(0xFF000000),
+//    errorContainer = Color(0xFFB00020),
+//    onErrorContainer = Color(0xFFFFFFFF),
+//
+//    background = Color(0xFF121212),
+//    onBackground = Color(0xFFFFFFFF),
+//    surface = Color(0xFF2B2B2B),
+//    onSurface = Color(0xFFFFFFFF),
+//    surfaceVariant = Color(0xFF525252),
+//    onSurfaceVariant = Color(0xFFE9E9E9),
+//
+//    outline = Color(0xFFCFD8DC),
+//    inverseSurface = Color(0xFFFFFFFF),
+//    inverseOnSurface = Color(0xFF000000),
+//    inversePrimary = Color(0xFFBB86FC)
+)
